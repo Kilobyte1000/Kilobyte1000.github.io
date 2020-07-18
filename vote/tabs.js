a3360daa18d729258bc61da0694c5f156b3cc705
Kilobyte1000 <59469337+Kilobyte1000@users.noreply.github.com>
2020-07-18 14:54:35 +0530

    All houses color

--- vote/tabs.js
+++ vote/tabs.js
@@ -1,1 +1,45 @@
-var sportsTab,houseTab,houseButton,sportsButton;function init(){houseTab=document.getElementById("house_prefect_tab"),sportsTab=document.getElementById("sports_prefect_tab"),houseButton=document.getElementById("houseButton"),sportsButton=document.getElementById("sportsButton"),houseButton.click()}function openTab(e,t){"house_prefect_tab"==t?(houseTab.className="visible",sportsTab.className="",sportsButton.className=""):(sportsTab.className="visible",houseTab.className="",houseButton.className=""),e.currentTarget.className="active"}function writeHouseBoy(e){document.getElementById("house_boy").innerHTML=e.nextElementSibling.innerHTML}function writeHouseGirl(e){document.getElementById("house_girl").innerHTML=e.nextElementSibling.innerHTML}function writeSportsBoy(e){document.getElementById("sports_boy").innerHTML=e.nextElementSibling.innerHTML}function writeSportsGirl(e){document.getElementById("sports_girl").innerHTML=e.nextElementSibling.innerHTML}
+var sportsTab, houseTab;
+var houseButton, sportsButton
+
+function init() {
+	houseTab = document.getElementById("house_prefect_tab");
+	sportsTab = document.getElementById("sports_prefect_tab");
+
+	houseButton = document.getElementById("houseButton");
+	sportsButton = document.getElementById("sportsButton");
+
+	houseButton.click();
+}
+
+function openTab(evt, tabName) {
+
+	if (tabName == "house_prefect_tab")
+	{
+		houseTab.className = "visible";
+		sportsTab.className = "";
+		sportsButton.className = "";
+	}
+	else
+	{
+		sportsTab.className = "visible";
+		houseTab.className = "";
+		houseButton.className = "";
+	}
+	evt.currentTarget.className = "active";
+}
+
+function writeHouseBoy(el) {
+	document.getElementById("house_boy").innerHTML = el.nextElementSibling.innerHTML;
+}
+
+function writeHouseGirl(el) {
+	document.getElementById("house_girl").innerHTML = el.nextElementSibling.innerHTML;
+}
+
+function writeSportsBoy(el) {
+	document.getElementById("sports_boy").innerHTML = el.nextElementSibling.innerHTML;
+}
+
+function writeSportsGirl(el) {
+	document.getElementById("sports_girl").innerHTML = el.nextElementSibling.innerHTML;
+}
